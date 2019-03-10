f4c018e170fbad38719c6813689d44822d7766fb
oriolaranda <oriolaranda@gmail.com>
2019-03-10 19:39:18 +0100

    oriol-commit

--- src/Map.java
+++ src/Map.java
@@ -47,10 +47,12 @@ public class Map {
 
 
     private boolean isCarFull(int c){
-        return false;
+        return numberPassengers(c) == 2;
     }
 
-
+    private boolean isCarEmpty(int c){
+        return numberPassengers(c) == 0;
+    }
 
     /** Public methods **/
     public boolean estaRecullit(int index)
@@ -86,18 +88,21 @@ public class Map {
 
 
 
-    /** Operator Swap Order of p and q in the same car **/
-    public boolean swapOrder(int c){
 
-        if (isCarFull(c)) {
+    /** Operator Swap Order of p1 and p2 in the same car c**/
+    public void swapOrder(int c){
 
+        if (numberPassengers(c) > 1) {
+            int p1 = getPassangers(c).get(0);
+            int p2 = getPassangers(c).get(1);
+            getPassangers(c).set(0,p2);
+            getPassangers(c).set(1,p1);
         }
-        return false;
     }
 
-    /** Operator Swap Car between p and q **/
-    public boolean swapCar(int p, int q) {
-        return false;
+    /** Operator Swap Car between p1 and p2 **/
+    public void swapCar(int p1, int p2, int c1, int c2) {
+
     }
 
     /** Operator Add Person p in car c **/
