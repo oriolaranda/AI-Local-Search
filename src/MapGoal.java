7f19e6c73c28ec35b620c7cb6fca96d0f18c3792
pol <polgarciarecasens@gmail.com>
2019-03-10 17:06:53 +0100

    pol

--- src/MapGoal.java
+++ src/MapGoal.java
@@ -2,5 +2,9 @@ package src;
 
 import aima.search.framework.GoalTest;
 
-public class MapGoal {
+public class MapGoal implements GoalTest{
+    public boolean isGoalState(Object aState){
+        boolean goal;
+
+    }
 }
