dc84194736693421ccc5348d629561f17808a1f4
David Pujol <david.pujolperich@gmail.com>
2019-03-10 17:17:06 +0100

    Inicialitzacio del projecte

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,15 @@
 package src;
 
+import java.util.ArrayList;
+import aima.util.Pair;
+
 public class Map {
+    /** Private atributes of the class **/
+    private ArrayList<Pair<Integer,ArrayList<Pair<Integer,Boolean>>>> estatConductors = new ArrayList<Pair<Integer, ArrayList<Pair<Integer, Boolean>>>>();
+    private ArrayList<Boolean> estaRecullit = new ArrayList<>();
+
+    public Map(){
+        //inicialitzem els usuaris
+    }
 
 }
