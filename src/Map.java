ce73b021a6f5c7d7c71b2c88fa3e504aa4247b33
David Pujol <david.pujolperich@gmail.com>
2019-03-10 17:38:41 +0100

    Inicialitzacio del projecte

--- src/Map.java
+++ src/Map.java
@@ -1,16 +1,35 @@
 package src;
 
 import java.util.ArrayList;
+
+import IA.Comparticion.*;
 import aima.util.Pair;
+import java.util.Random;
 
 public class Map {
     /** Private atributes of the class **/
     private ArrayList<Pair> estatConductors = new ArrayList<>();
     private ArrayList<Boolean> estaRecullit = new ArrayList<>();
+    private Usuarios nouUsuaris = new Usuarios(100,50, 2);
 
+    /** Constructor **/
     public Map(){
-        //inicialitzem els usuaris
+        ompleEstatConductors ();
+        ompleEsta
     }
+
+
+    /** Private methods **/
+    private ompleEstatConductors() {
+        int n = nouUsuaris.size();
+        estaRecullit = new ArrayList<boolean> (n);
+        for (int i=0; i < n)
+    }
+
+
+
+    /** Public methods **/
+
 
     public boolean isGoal(){
         return true;
