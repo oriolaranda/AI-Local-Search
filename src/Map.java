8038d071d802efc0e5a9aae61749f57946ef6304
David Pujol <david.pujolperich@gmail.com>
2019-03-10 17:24:18 +0100

    Inicialitzacio del projecte

--- src/Map.java
+++ src/Map.java
@@ -5,7 +5,7 @@ import aima.util.Pair;
 
 public class Map {
     /** Private atributes of the class **/
-    private ArrayList<Pair<Integer,ArrayList<Pair<Integer,Boolean>>>> estatConductors = new ArrayList<Pair<Integer, ArrayList<Pair<Integer, Boolean>>>>();
+    private ArrayList<Pair> estatConductors = new ArrayList<>();
     private ArrayList<Boolean> estaRecullit = new ArrayList<>();
 
     public Map(){
