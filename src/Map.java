a4f0a1e1cff5cc68f85122c2b06883774da857ba
pol <polgarciarecasens@gmail.com>
2019-03-17 17:36:27 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Map.java

--- src/Map.java
+++ src/Map.java
@@ -167,19 +167,6 @@ public class Map {
 
 
 
-    public boolean swap(int i, int j, int c){
-
-        ArrayList a = (ArrayList)estatConductors.get(c).getSecond();
-        int a1 = (Integer)a.get(i);
-        int b1 = (Integer)a.get(j);
-        a.set(i,a1);
-        a.set(j,b1);
-        Pair p = new Pair(estatConductors.get(c).getFirst(),a);
-        estatConductors.set(c,p);
-        return true;
-
-    }
-
     /**Function to find distance from a given passanger assignation **/
     /** The calculus will be made in hundred meters units, so there is a maximum of 300 per passenger **/
     public int calculateDistance(int indexCar, ArrayList<Integer> passangers)
@@ -230,20 +217,19 @@ public class Map {
 
 
     /** Operator Swap Order of p1 and p2 in the same car c**/
-    public void swapOrder(int p1, int p2, int c){
+    public boolean swapOrder(int i, int j, int c){
 
-        int q1 = getPassangers(c).indexOf(p1);
-        int q2 = getPassangers(c).indexOf(p2);
-        if(q1 >= 0 || q2 >= 0) {
-            getPassangers(c).set(q1,p2);
-            getPassangers(c).set(q2,p1);
-            q1 = getPassangers(c).indexOf(p1);
-            q2 = getPassangers(c).indexOf(p2);
-            getPassangers(c).set(q1,p2);
-            getPassangers(c).set(q2,p1);
-        }
-        int newDist = calculateDistance(c,getPassangers(c));
-        setDistance(c,newDist);
+        ArrayList a = (ArrayList)estatConductors.get(c).getSecond();
+        int a1 = (Integer)a.get(i);
+        int b1 = (Integer)a.get(j);
+        a.set(j,a1);
+        a.set(i,b1);
+        int k = calculateDistance(c,a);
+        Pair p1 = new Pair(k,c);
+        Pair p = new Pair(p1,a);
+        estatConductors.set(c,p);
+        return true;
+
     }
 
     /** Operator Swap Car between p1 in c1 and p2 in c2 **/
