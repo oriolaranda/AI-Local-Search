ad7e5e10f9c659035e4c52bfd82b74687fbac712
David Pujol <david.pujolperich@gmail.com>
2019-03-17 18:29:46 +0100

    He afegit la funcio removeDriver

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,18 @@
 package src;
 
 import IA.Comparticion.Usuarios;
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
 import src.*;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
-private static void MapHillClimbing1(Map m) {
-
-        try {
-        Problem problem =  new Problem(m,new MapSuccesors(), new MapGoal());
-        Search search =  new DepthLimitedSearch(limit);
-        SearchAgent agent = new SearchAgent(problem,search);
-
-        System.out.println();
-        printActions(agent.getActions());
-        printInstrumentation(agent.getInstrumentation());
-        } catch (Exception e) {
-        e.printStackTrace();
-        }
-}
 
 public class Main {
 
@@ -38,7 +30,38 @@ public class Main {
     }
 
 
+    private static void MapHillClimbing1(Map m) {
 
+        try {
+            Problem problem =  new Problem(m,new MapSuccesors(), new MapGoal());
+            Search search =  new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent(problem,search);
+
+            System.out.println();
+            printActions(agent.getActions());
+        //    printInstrumentation(agent.getInstrumentation());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void printInstrumentation(Properties properties) {
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
+
+    }
+
+    private static void printActions(List actions) {
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
+    }
 
 
     /**
