f64b89f42ef14c55f4bd2e7e9d340fda6cb2ecdb
oriolaranda <oriolaranda@gmail.com>
2019-03-17 02:28:13 +0100

    oriol-commit

--- src/GUIForm.java
+++ src/GUIForm.java
@@ -1,7 +1,14 @@
 package src;
 
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.text.ParseException;
 
 public class GUIForm extends JFrame
 {
@@ -17,8 +24,8 @@ public class GUIForm extends JFrame
     private JLabel estatsSolucio;
     private JLabel heuristics;
     private JButton executar;
-    private JTextField mInput;
     private JTextField nInput;
+    private JTextField mInput;
     private JLabel nValue;
     private JLabel mValue;
     private JLabel footer;
@@ -32,10 +39,43 @@ public class GUIForm extends JFrame
         setTitle("GUIFORM");
         setSize(700,450);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        //Initial values for n and m
+        nInput.setText("100");
+        mInput.setText("50");
+
+
+        //Execute algorithm!
+        executar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (nInput.getText().length() == 0 || mInput.getText().length() == 0)
+                    JOptionPane.showMessageDialog(rootPanel,"Falta algun camp per omplir", "Error", JOptionPane.WARNING_MESSAGE);
+                else {
+                    int n = Integer.parseInt(nInput.getText());
+                    int m = Integer.parseInt(mInput.getText());
+                }
+            }
+        });
+
+        //JTexFields KeyListeners
+        nInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                numberFormatter(e, nInput);
+            }
+        });
+
+        mInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                numberFormatter(e, mInput);
+            }
+        });
     }
 
 
-    private void createUIComponents() {
+    private void createUIComponents() throws ParseException {
         // TODO: place custom component creation code here
 
         //ButtonGroup radio buttons
@@ -51,5 +91,14 @@ public class GUIForm extends JFrame
 
         /* Canvas ??*/
         //mapa = new JPanel();
+    }
+
+    //Only numbers for JTextFields
+    private void numberFormatter(KeyEvent evt, JTextField input)
+    {
+        if(!Character.isDigit(evt.getKeyChar()) || input.getText().length()>= 3){
+            evt.consume();
+            getToolkit().beep();
+        }
     }
 }
