863789a6f1193c7cdc809b5ba37cf8ca80cdac37
David Pujol <david.pujolperich@gmail.com>
2019-03-17 20:03:09 +0100

    He afegit la funcio removeDriver

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static ArrayList<Boolean> isConductor = new ArrayList<>(); //This must be global
     public static Usuarios nouUsuaris;    //this must be global
-    public static int n = 200;
+    public static int n = 100;
     public static int m = 50;
     public static int seed = 2;
 
@@ -36,31 +36,30 @@ public class Main {
     private static void MapHillClimbing1(Map m) {
 
         try {
-            Problem problem =  new Problem(m,new MapSuccesors(), new MapGoal(), new Heuristic1());
-            Search search =  new HillClimbingSearch();
-            SearchAgent agent = new SearchAgent(problem,search);
+            Problem problem = new Problem(m, new MapSuccesors(), new MapGoal(), new Heuristic1());
+            Search search = new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent(problem, search);
 
             System.out.println();
             printActions(agent.getActions());
             printInstrumentation(agent.getInstrumentation());
-            printSolution((Map)search.getGoalState());
+            printSolution((Map) search.getGoalState());
 
+            System.out.println(checkSolution((Map) search.getGoalState()));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void printSolution(Map a)
-    {
+    private static void printSolution(Map a) {
         ArrayList<Pair> b = a.getEstatConductors();
-        for (Pair c : b)
-        {
-            Pair d = (Pair)c.getFirst();
-            System.out.println("El conductor "+d.getSecond()+" fa "+d.getFirst());
+        for (Pair c : b) {
+            Pair d = (Pair) c.getFirst();
+            System.out.println("El conductor " + d.getSecond() + " fa " + d.getFirst());
             System.out.println("Ordre de recollida/arribada de passatgers");
-            ArrayList<Integer> e = (ArrayList<Integer>)c.getSecond();
-            for(Integer p: e)
-                System.out.print(p+" ");
+            ArrayList<Integer> e = (ArrayList<Integer>) c.getSecond();
+            for (Integer p : e)
+                System.out.print(p + " ");
             System.out.println("\n");
         }
     }
@@ -84,7 +83,6 @@ public class Main {
     }
 
 
-
     /**
      * Private methods
      **/
@@ -93,4 +91,18 @@ public class Main {
             isConductor.add(nouUsuaris.get(i).isConductor());
     }
 
+    private static boolean checkSolution(Map a) {
+        ArrayList<Pair> b = a.getEstatConductors();
+        for (Pair c : b)    //iterate over all drivers
+        {
+            Pair d = (Pair) c.getFirst();
+            if ((Integer) d.getFirst() > 300) return false;
+        }
+
+        ArrayList<Boolean> c = a.getEstaRecullit();
+        for (Boolean r : c)
+            if (!r) return false;
+
+        return true;
+    }
 }
