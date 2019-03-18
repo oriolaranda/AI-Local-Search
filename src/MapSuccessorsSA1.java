7b9b345353572a9857047e08346120955b0f1ade
David Pujol <david.pujolperich@gmail.com>
2019-03-18 23:45:01 +0100

    Fent les funcions de successors de simulated annealing

--- src/MapSuccessorsSA1.java
+++ src/MapSuccessorsSA1.java
@@ -24,6 +24,8 @@ public class MapSuccessorsSA1 {
             //We don't wanna generate all possibilities but one using random function
             Random myRandom=new Random();
             int option = myRandom.nextInt(4);
+            int c,p1,p2;
+            Map aux;
 
             switch (option) {
 
@@ -35,8 +37,8 @@ public class MapSuccessorsSA1 {
 
                     if (!estaRecullit.get(person) && !isConductor.get(person))
                     {
-                        int c = myRandom.nextInt(map.getEstatConductors().size());
-                            Map aux = new Map(map); // copy of map
+                        c = myRandom.nextInt(map.getEstatConductors().size());
+                            aux = new Map(map); // copy of map
                             aux.setEstaRecullit(person,true);
                             aux.addPerson(person,c);
                             retVal.add(new Successor(new String("Afegim una persona al cotxe"+c), aux));
@@ -50,10 +52,10 @@ public class MapSuccessorsSA1 {
 
                 case(1):
                     //REMOVE PERSON
-                    int c = myRandom.nextInt(map.getEstatConductors().size());
+                    c = myRandom.nextInt(map.getEstatConductors().size());
                     int k = myRandom.nextInt(map.getPassangers(c).size());  //i get a random passanger
 
-                    Map aux = new Map(map);
+                    aux = new Map(map);
                     aux.rmPerson(k,c);
                     retVal.add(new Successor(new String("Borrem una persona del cotxe"+c), aux));
 
@@ -63,54 +65,33 @@ public class MapSuccessorsSA1 {
                 case(2):
                     //SWAP CAR
                     int c1 = myRandom.nextInt(map.getEstatConductors().size());
-                    int p1 = myRandom.nextInt(map.getPassangers(c1).size());
+                    p1 = myRandom.nextInt(map.getPassangers(c1).size());
 
                     int c2 = myRandom.nextInt(map.getEstatConductors().size());
-                    int p2 = myRandom.nextInt(map.getPassangers(c2).size());
+                    p2 = myRandom.nextInt(map.getPassangers(c2).size());
 
-                    Map aux2 = new Map(map);
-                    aux2.swapCar(p1,p2,c1,c2);
-                    retVal.add(new Successor(new String("Fem swap de les persones "+p1+" "+p2+" dels cotxes "+c1 + " i "+c2), aux2));
+                    aux = new Map(map);
+                    aux.swapCar(p1,p2,c1,c2);
+                    retVal.add(new Successor(new String("Fem swap de les persones "+p1+" "+p2+" dels cotxes "+c1 + " i "+c2), aux));
 
-                                }
-                            }
-                        }
-                    }
                 break;
 
                 default:
                     //FALTARA EL SWAP ORDER
-                    for (int c=0; c < map.getEstatConductors().size(); ++c)
+                    c = myRandom.nextInt(map.getEstatConductors().size());
+
+                    p1 = myRandom.nextInt(map.getPassangers(c).size()*2);
+                    p2 = myRandom.nextInt(map.getPassangers(c).size()*2);
+
+                    aux = new Map(map); // copy of map
+
+                    if (aux.swapOrder(p1,p2,c))   //we enter only if the operation is not between the same person
                     {
-                        HashSet<Integer> p1 = uniquePassengers.get(c);
-                        for (int j=0; j < p1.size()*2; ++j)
-                        {
-                            for (int k=j+1; k < p1.size()*2; ++k)
-                            {
-                                Map aux = new Map(map); // copy of map
-                                if (aux.swapOrder(j,k,c))   //we enter only if the operation is not between the same person
-                                {
-                                    retVal.add(new Successor(new String("Fem swap order del cotxe "+c+ " dels passatgers "+j+" i "+k), aux));
-                                }
-                            }
-                        }
+                        retVal.add(new Successor(new String("Fem swap order del cotxe "+c+ " dels passatgers "+p1+" i "+p2), aux));
                     }
+
+
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             return retVal;
         }
@@ -128,7 +109,5 @@ public class MapSuccessorsSA1 {
             return a;
         }
 
-
-    }
 
 }
